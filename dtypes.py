724c6702fc2ce2fd056365da043368586f986e96
Paul Westin <pilate@pilate.es>
2014-03-15 00:57:23 -0700

    fix tabs

--- dtypes.py
+++ dtypes.py
@@ -17,49 +17,49 @@ typeNames = {
 """
 
 class dtype(object):
-	struct_fmt = ""
-	read_len = 1
+    struct_fmt = ""
+    read_len = 1
 
-	@classmethod
-	def unpack(class_obj, stream):
-		read_len = struct.calcsize(class_obj.struct_fmt)
-		read_data = stream.read(read_len)
-		return struct.unpack(class_obj.struct_fmt, read_data)[0]
+    @classmethod
+    def unpack(class_obj, stream):
+        read_len = struct.calcsize(class_obj.struct_fmt)
+        read_data = stream.read(read_len)
+        return struct.unpack(class_obj.struct_fmt, read_data)[0]
 
-	@classmethod
-	def pack(class_obj, value):
-		try:
-			iter(value)
-		except:
-			value = [value]
-		return struct.pack(class_obj.struct_fmt, *value)
+    @classmethod
+    def pack(class_obj, value):
+        try:
+            iter(value)
+        except:
+            value = [value]
+        return struct.pack(class_obj.struct_fmt, *value)
 
 class uint8(dtype):
-	struct_fmt = "B"
+    struct_fmt = "B"
 
 class uint16(dtype):
-	struct_fmt = "H"
+    struct_fmt = "H"
 
 class uint32(dtype):
-	struct_fmt = "I"
+    struct_fmt = "I"
 
 class uint64(dtype):
-	struct_fmt = "Q"
+    struct_fmt = "Q"
 
 class int32(dtype):
-	struct_fmt = "i"
+    struct_fmt = "i"
 
 class varint(dtype):
-	@classmethod
-	def unpack(class_obj, stream):
-		read_data = uint8.unpack(stream)
-		if read_data < 0xfd:
-			return read_data
-		elif read_data == 0xfd:
-			return uint16.unpack(stream)
-		elif read_data == 0xfe:
-			return uint32.unpack(stream)
-		elif read_data == 0xff:
-			return uint64.unpack(stream)
-		else:
-			return -1
+    @classmethod
+    def unpack(class_obj, stream):
+        read_data = uint8.unpack(stream)
+        if read_data < 0xfd:
+            return read_data
+        elif read_data == 0xfd:
+            return uint16.unpack(stream)
+        elif read_data == 0xfe:
+            return uint32.unpack(stream)
+        elif read_data == 0xff:
+            return uint64.unpack(stream)
+        else:
+            return -1
